67f12e070489a2599fddcf21d9246db38fcdb44b
anngreen <anngreen@qq.com>
2023-11-05 10:57:07 +0800

    elementplus

--- src/router/routes/dev.js
+++ src/router/routes/dev.js
@@ -3,16 +3,6 @@ const commonroute = [
     path: "/dev",
     name: "dev",
     component: () => import("@/views/Dev/CommonRoute.vue"),
-  },
-  {
-    path: "/dev/SearchMatchA",
-    name: "dev-SearchMatchA",
-    component: () => import("@/views/Dev/SearchMatchA.vue"),
-  },
-  {
-    path: "/dev/SearchMatchB",
-    name: "dev-SearchMatchB",
-    component: () => import("@/views/Dev/SearchMatchB.vue"),
-  },
+  }
 ];
 export default commonroute;
